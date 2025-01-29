f5ebea2f55bc95a31bbb0e27d43e57fd60f7601c
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2025-01-29 19:41:28 +0530

    change frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,63 @@
-import React, { useState, useRef } from "react";
-import DataTable from "./DataTable";
-import logic from "./logic";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import CalculatePage from "./CalculatePage";
+import Home from "./Home";
 
 const App = () => {
-  const [principalAmount, setPrincipalAmount] = useState("");
-  const [rate, setRate] = useState("");
-  const [targetAmount, setTargetAmount] = useState("");
-  const [data, setData] = useState([]);
-
-  const inputRef = useRef(null);
-
-  const formatRupees = (value) => {
-    // Remove commas and other non-digit characters from the value
-    const cleanedValue = value.replace(/\D/g, "");
-    // Parse the cleaned value as an integer
-    const parsedValue = parseInt(cleanedValue, 10);
-    // Format the parsed value as Indian Rupees without decimals
-    return new Intl.NumberFormat("en-IN", {
-      style: "decimal",
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    }).format(parsedValue);
-  };
-
-  function handleAddData(e) {
-    e.preventDefault(); // Prevent default form submission behavior
-
-    const newData = logic(principalAmount, rate, targetAmount);
-    setData(newData);
-    setPrincipalAmount("");
-    setRate("");
-    setTargetAmount("");
-  }
-
   return (
-    <div className="p-8">
-      <div className="w-full rounded-md border border-gray-300 p-8 bg-white">
-        <h2 className="mb-4 text-2xl font-semibold">Aim of Achieve:</h2>
-
-        <form
-          className="flex w-full flex-row flex-wrap items-start justify-between gap-4"
-          onSubmit={handleAddData}
-        >
-          <div className="w-full p-1 md:w-64">
-            <label
-              htmlFor="principle_input_data"
-              className="mb-1 block font-semibold text-gray-700"
-            >
-              Principle Amount:
-            </label>
-            <input
-              type="text"
-              id="principle_input_data"
-              placeholder="Principle Amount"
-              name="principle_input_data"
-              required
-              value={
-                principalAmount !== "" ? formatRupees(principalAmount) : ""
-              }
-              onChange={(e) => {
-                // Remove commas and other non-digit characters from the input value
-                const cleanedValue = e.target.value.replace(/\D/g, "");
-                // Update the state with the cleaned numeric value
-                setPrincipalAmount(cleanedValue);
-              }}
-              className="w-full rounded border border-gray-300 p-1 font-normal"
-              ref={inputRef}
-            />
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        {/* Navbar */}
+        <nav className="bg-sky-100 py-4 px-6 shadow-md">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-xl font-bold">My Smile</h1>
+            <ul className="flex gap-6">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `px-4 py-2 rounded-md font-bold ${
+                      isActive
+                        ? "underline decoration-blue-600 text-blue-600 "
+                        : "hover:text-blue-600 font-bold"
+                    }`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/calculate"
+                  className={({ isActive }) =>
+                    `px-4 py-2 rounded-md font-bold ${
+                      isActive
+                        ? "underline decoration-blue-600 text-blue-600 "
+                        : "hover:text-blue-600 font-bold"
+                    }`
+                  }
+                >
+                  Calculate
+                </NavLink>
+              </li>
+            </ul>
           </div>
+        </nav>
 
-          <div className="w-full p-1 md:w-64">
-            <label
-              htmlFor="rate_input_data"
-              className="mb-1 block font-semibold text-gray-700"
-            >
-              Rate:
-            </label>
-            <input
-              type="text"
-              id="rate_input_data"
-              placeholder="Rate Amount"
-              name="rate_input_data"
-              required
-              value={rate}
-              onChange={(e) => setRate(e.target.value)}
-              className="w-full rounded border border-gray-300 p-1 font-normal"
-            />
-          </div>
-
-          <div className="w-full p-1 md:w-64">
-            <label
-              htmlFor="target_input_data"
-              className="mb-1 block font-semibold text-gray-700"
-            >
-              Target Amount:
-            </label>
-            <input
-              type="text"
-              id="target_input_data"
-              placeholder="Target Amount"
-              name="target_input_data"
-              required
-              value={targetAmount !== "" ? formatRupees(targetAmount) : ""}
-              onChange={(e) => {
-                // Remove commas and other non-digit characters from the input value
-                const cleanedValue = e.target.value.replace(/\D/g, "");
-                // Update the state with the cleaned numeric value
-                setTargetAmount(cleanedValue);
-              }}
-              className="w-full rounded border border-gray-300 p-1 font-normal"
-            />
-          </div>
-
-          <div className="w-full p-1 md:w-24">
-            <button
-              type="submit"
-              className="w-full h-10 rounded-lg bg-blue-600 text-white"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+        {/* Page Routes */}
+        <div className="container mx-auto mt-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculate" element={<CalculatePage />} />
+          </Routes>
+        </div>
       </div>
-
-      <DataTable data={data} />
-    </div>
+    </Router>
   );
 };
 
---
+++ src/CalculatePage.jsx
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+
+const CalculatePage = () => {
+  const [buyPrice, setBuyPrice] = useState("");
+  const [rate, setRate] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [sellDetails, setSellDetails] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Convert input values to float
+    const buyPriceFloat = parseFloat(parseFloat(buyPrice).toFixed(2));
+    const rateFloat = parseFloat(parseFloat(rate).toFixed(2));
+    const quantityInt = parseInt(quantity, 10); // Ensure base 10 parsing
+
+    if (isNaN(buyPriceFloat) || isNaN(rateFloat) || isNaN(quantityInt)) {
+      alert("Please enter valid numerical values.");
+      return;
+    }
+
+    // Calculate sell price
+    const sellPrice = parseFloat(
+      (buyPriceFloat + (buyPriceFloat * rateFloat) / 100).toFixed(2)
+    );
+
+    const differenceBetweenSellAndBuy = parseFloat(
+      (sellPrice - buyPriceFloat).toFixed(2)
+    );
+
+    // Calculate profit
+    const profit = parseFloat(
+      (differenceBetweenSellAndBuy * quantityInt).toFixed(2)
+    );
+
+    // Update state
+    setSellDetails({
+      buyPrice: buyPriceFloat,
+      sellPrice,
+      profit,
+      differenceBetweenSellAndBuy,
+      sellRate: rateFloat,
+    });
+  };
+
+  return (
+    <div className="p-8">
+      <div className="w-full rounded-md border border-gray-300 p-8 bg-white">
+        <h2 className="mb-4 text-2xl font-semibold">Aim of Achieve Rate:</h2>
+
+        <form
+          className="flex w-full flex-row flex-wrap items-start justify-between gap-4"
+          onSubmit={handleSubmit}
+        >
+          {/* Buy Price Input */}
+          <div className="w-full p-1 md:w-64">
+            <label
+              htmlFor="buy_price"
+              className="mb-1 block font-semibold text-gray-700"
+            >
+              Buy Price
+            </label>
+            <input
+              type="text"
+              id="buy_price"
+              placeholder="Enter Buy Price"
+              name="buy_price"
+              required
+              value={buyPrice}
+              onChange={(e) => {
+                const cleanedValue = e.target.value.replace(/[^0-9.]/g, ""); // Allow only numbers and dots
+                setBuyPrice(cleanedValue);
+              }}
+              className="w-full rounded border border-gray-300 p-1 font-normal"
+            />
+          </div>
+
+          {/* Target Rate Input */}
+          <div className="w-full p-1 md:w-64">
+            <label
+              htmlFor="rate"
+              className="mb-1 block font-semibold text-gray-700"
+            >
+              Target Rate:
+            </label>
+            <input
+              type="text"
+              id="rate"
+              placeholder="Enter Target Rate"
+              name="rate"
+              required
+              value={rate}
+              onChange={(e) => {
+                const cleanedValue = e.target.value.replace(/[^0-9.]/g, ""); // Allow only numbers and dots
+                setRate(cleanedValue);
+              }}
+              className="w-full rounded border border-gray-300 p-1 font-normal"
+            />
+          </div>
+
+          {/* Quantity Input */}
+          <div className="w-full p-1 md:w-64">
+            <label
+              htmlFor="quantity"
+              className="mb-1 block font-semibold text-gray-700"
+            >
+              Quantity
+            </label>
+            <input
+              type="text"
+              id="quantity"
+              placeholder="Enter Quantity"
+              name="quantity"
+              required
+              value={quantity}
+              onChange={(e) => {
+                const cleanedValue = e.target.value.replace(/[^0-9.]/g, ""); // Allow only numbers and dots
+                setQuantity(cleanedValue);
+              }}
+              className="w-full rounded border border-gray-300 p-1 font-normal"
+            />
+          </div>
+
+          {/* Submit Button */}
+          <div className="w-full p-1 md:w-24">
+            <button
+              type="submit"
+              className="w-full h-10 rounded-lg bg-blue-600 text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      {sellDetails !== null ? (
+        <div className="w-full rounded-md border border-gray-300 p-8 bg-white mt-4">
+          <h2 className="mb-4 text-2xl font-semibold text-blue-600">
+            Aim to sell price:
+          </h2>
+
+          <div className="flex w-full flex-row flex-wrap items-start justify-between gap-4">
+            <div className="w-full p-1 md:w-64">
+              <p className="mb-1 block font-semibold text-gray-700">
+                Buy Price:
+              </p>
+              <p className="w-full rounded border border-gray-300 p-1 font-normal">
+                {sellDetails.buyPrice}
+              </p>
+            </div>
+            <div className="w-full p-1 md:w-64">
+              <p className="mb-1 block font-semibold text-gray-700">
+                Sell Rate:
+              </p>
+              <p className="w-full rounded border border-gray-300 p-1 font-normal">
+                {sellDetails.sellRate}
+              </p>
+            </div>
+            <div className="w-full p-1 md:w-64">
+              <p className="mb-1 block font-semibold text-gray-700">
+                Sell Price:
+              </p>
+              <p className="w-full rounded border border-gray-300 p-1 font-normal">
+                {sellDetails.sellPrice}
+              </p>
+            </div>
+            <div className="w-full p-1 md:w-64">
+              <p className="mb-1 block font-semibold text-gray-700">Gain:</p>
+              <p className="w-full rounded border border-gray-300 p-1 font-normal">
+                {sellDetails.differenceBetweenSellAndBuy}
+              </p>
+            </div>
+            <div className="w-full p-1 md:w-64">
+              <p className="mb-1 block font-semibold text-gray-700">Profit:</p>
+              <p className="w-full rounded border border-gray-300 p-1 font-normal">
+                {sellDetails.profit}
+              </p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default CalculatePage;
